f898b3052a16649216799b161251dee5c2f2e90a
Katie Grubbs <katie.grubbs@gmail.com>
2017-08-27 21:32:08 -0400

    Added Ingredient, Cocktail, and Order Models

---
+++ BartenderApplication/Models/Cocktail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BartenderApplication.Models
+{
+    public class Cocktail
+    {
+        public int Id { get; set; }
+        public string CocktailName { get; set; }
+
+        // Each Cocktail has an optional association with an Order
+        public int? OrderId { get; set; }
+        public Order Order { get; set; }
+
+        public ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
---
+++ BartenderApplication/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BartenderApplication.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string OrderedBy { get; set; }
+        public DateTime OrderTime { get; set; }
+        public ICollection<Cocktail> Cocktails { get; set; }
+    }
+}
